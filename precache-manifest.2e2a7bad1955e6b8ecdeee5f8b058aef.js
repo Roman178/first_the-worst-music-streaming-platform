d352ebe672d555ba092ed471ff7bc308eddcbd08
Roman <ismagilov771@gmail.com>
2021-02-27 22:56:00 +0300

    Updates

---
+++ precache-manifest.2e2a7bad1955e6b8ecdeee5f8b058aef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "36a351efe155ab8ab2a7",
+    "url": "/first_the-worst-music-streaming-platform/static/css/main.f587b0f2.chunk.css"
+  },
+  {
+    "revision": "36a351efe155ab8ab2a7",
+    "url": "/first_the-worst-music-streaming-platform/static/js/main.5a235b32.chunk.js"
+  },
+  {
+    "revision": "34dd773bb6b2fd47c698",
+    "url": "/first_the-worst-music-streaming-platform/static/js/runtime~main.9f86305b.js"
+  },
+  {
+    "revision": "43d9c1b59a78516e50d8",
+    "url": "/first_the-worst-music-streaming-platform/static/js/2.e6f98898.chunk.js"
+  },
+  {
+    "revision": "ae1cceefd49178f91ff99c1beeb951d8",
+    "url": "/first_the-worst-music-streaming-platform/static/media/first.ae1cceef.png"
+  },
+  {
+    "revision": "7af37e2351abeca8d7881f0e12e60d27",
+    "url": "/first_the-worst-music-streaming-platform/static/media/second.7af37e23.png"
+  },
+  {
+    "revision": "c56dd13d6edc992fe109f054bf2d04e1",
+    "url": "/first_the-worst-music-streaming-platform/static/media/third.c56dd13d.png"
+  },
+  {
+    "revision": "5e30b27b35e0deef6499ff913619dd49",
+    "url": "/first_the-worst-music-streaming-platform/index.html"
+  }
+];
